feb4839250f25727a10f4e45bd57cdcfbcb4375f
Ekaterina <estambolieva@gmail.com>
2017-02-17 14:20:34 +0000

    map bounding. actual binbag data

--- helper/csv_to_geojson.py
+++ helper/csv_to_geojson.py
@@ -1,10 +1,40 @@
 import csv, os
+import requests
+from geopy.geocoders import Nominatim
+
+# using Google Maps API
+def get_lat_lon(address, locality, city):
+  response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA')
+  resp_json_payload = response.json()
+  latitude = resp_json_payload['results'][0]['geometry']['location'].get('lat')
+  longitude = resp_json_payload['results'][0]['geometry']['location'].get('lng')
+  return latitude, longitude
+
+# using geopy library
+def get_lat_lon_geopy(address, locality, city):
+  geolocator = Nominatim()
+  try:
+    location = geolocator.geocode(address + ", " + locality + ", " + city)
+    print('location 1', location)
+    return location.latitude, location.longitude
+  except:
+    try:
+      location = geolocator.geocode(locality + ", " + city)
+      print('location 2', location)
+      return location.latitude, location.longitude
+    except:
+      try:
+        location = geolocator.geocode(city)
+        print('location 3', location)
+        return location.latitude, location.longitude
+      except:
+        return 0,0
 
 # Read in raw data from csv
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 
-rawData = csv.reader(open(parent_dir + '/data/binbag/sweetgreen.csv', 'r'), dialect='excel')
+rawData = csv.reader(open(parent_dir + '/data/binbag/recyclers.csv', 'r'), dialect='excel')
 
 # the template. where data from the csv will be formatted to geojson
 template = \
@@ -19,15 +49,15 @@ template = \
               ]
             },
             "properties": {
-              "name": "%s",
-              "phoneFormatted": "%s",
-              "phone": "%s",
-              "address": "%s",
+              "company": "%s",
+              "type": "%s",
+              "plot": "%s",
+              "stage": "%s",
+              "industrial": "%s",
+              "locality": "%s",
               "city": "%s",
-              "country": "%s",
-              "crossStreet": "%s",
-              "postalCode": "%s",
-              "state": "%s"
+              "state": "%s",
+              "country": "%s"
             }
           },
     '''
@@ -45,21 +75,19 @@ iter = 0
 for row in rawData:
     iter += 1
     if iter >= 2:
-        name = row[0]
-        longitude = row[1]
-        latitude = row[2]
-        altitude = row[3]
-        geometry = row[4]
-        phoneFormatted = row[5]
-        phone = row[6]
-        address = row[7]
-        city = row[8]
+        company_id= row[0]
+        company = row[1]
+        waste_type = row[2]
+        plot = row[3]
+        stage = row[4]
+        industrial_area = row[5]
+        locality = row[6]
+        city = row[7]
+        state = row[8]
         country = row[9]
-        crossStreet = row[10]
-        postalCode = row[11]
-        state = row[12]
-        output += template % (latitude, longitude, name, phoneFormatted, phone, address, city, country, crossStreet, postalCode, state)
-        # output += template % (latitude, longitude)
+        # latitude, longitude = get_lat_lon(locality, city)
+        latitude, longitude = get_lat_lon_geopy(industrial_area, locality, city)
+        output += template % (longitude, latitude, company, waste_type, plot, stage, industrial_area, locality, city, state, country)
 
 # replace the comma after the last '},'
 output = output[:-6]
